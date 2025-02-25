a7a95647e068166b541d2b65336e33240c474698
archana875 <archangade478@gmail.com>
2025-02-25 06:40:08 -0800

    public folder added

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import React from 'react'
+import "./App.css"
+function App() {
+  return (
+    <div>
+      <h1 className='font-mono text-4xl font-semibold text-cyan-700 text-center m-px'>RAHURI</h1>
+      <img src="https://rahurisemenstation.com/wp-content/uploads/2022/12/Rahuri-Gate-scaled.jpg" className='h-74 rounded-xl m-auto mt-8'/>
+      <p className='mt-8 mx-32 text-xl '><span class='text-center'>History About Rahuri</span><br>
+      </br>Rahuri is a taluka in the Ahmednagar district of Maharashtra, India. It is known for its agricultural heritage, educational institutions, and cultural richness. 
+      According to legend, the town was named Rahuri, after Rahu, a historical figure in Hinduism.[3] It is believed that Rahu's head touched the ground at this place after it was beheaded by Mohini (Vishnu) from Newasa. Rahuri also has the second most important temple of the Hindu God Shani Dev.</p>
+    </div>
+  )
+}
+
+export default App
---
+++ src/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import Card from "./Card"
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <>
+    <App />
+    <div className='flex justify-evenly min-h-<fraction> rounded-sm font-mono'>
+        <div className=''>
+    <Card title="Mula-Dam" image="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEic6Wdkpqe0wJWuhxe1q9UtGMMa8J56WoReR_4xy2sdD326Sr0ET8pRZfGgsmaCmNCfwXnuGwDf-Q5-YsoOIq5uh7saE7hjRnyMdBnvh7NuK7NFhyPiJis_oSBj79K5GK-eAvD4wf80q8c/s1600/Mula.jpg" />
+    </div>
+     <div className='justify-center '>
+    <Card title="MPKV" image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRkTubWmz_CjddXPboueAK2NECFYn_Ldg8W6w&s"/>
+    </div>
+    </div>
+    <div className='flex justify-evenly min-h-<fraction> rounded-sm font-mono'>
+        <div className=''>
+    <Card title="ASC-College" image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQanrm4N7p-W8r34sm1yQX3tOCNkAPlkTaNLg&s" />
+    </div>
+     <div className='justify-center '>
+    <Card title="Shani-Temple" image="https://bhatkantiholidays.com/wp-content/uploads/2020/10/Shani-Shingnapur-4.jpg"/>
+    </div>
+    </div>
+    </>
+
+);
